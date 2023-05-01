fb12d62af4fb1c1ed14021c8f64b5129b72d1e54
DESKTOP-N269UEU\1 <734005065@qq.com>
2023-05-02 01:37:48 +0800

    顾客随机

--- Assets/Script/Controllers/CustomImageController.cs
+++ Assets/Script/Controllers/CustomImageController.cs
@@ -5,15 +5,14 @@ using UnityEngine.UI;
 
 public class CustomImageController : MonoBehaviour
 {
-    public Image[] images;  // 图片数组
+    public Image image;
+    public string CustomImage;
+    private Sprite[] images;
 
-    private void Start()
+    public void OnClick()
     {
-        // 随机生成一个 0 到 2 之间的整数，用来决定生成哪一个图片
-        int randomIndex = Random.Range(0, 4);
-
-        // 根据随机数生成相应的图片
-        images[randomIndex].gameObject.SetActive(true);
+        images = Resources.LoadAll<Sprite>(CustomImage);
+        image.sprite = images[Random.Range(0, 4)];
     }
 }
 
