4210bbdd91fa3b329e344c3f6d7c56740c334e73
Zhou YD <playerzhou@icloud.com>
2023-05-02 06:02:43 +0800

    add hover

--- Assets/Script/Controllers/DishDisplay.cs
+++ Assets/Script/Controllers/DishDisplay.cs
@@ -10,6 +10,7 @@ public class DishDisplay : MonoBehaviour
     public Image originalImage;
     public Image plateImage;
     public Image visionImage;
+    public Image hover;
     public List<Sprite> visionImages;
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,7 @@ public class DishDisplay : MonoBehaviour
     public void UpdateData(FoodData data)
     {
         dish = data;
-
+        hover.sprite = dish.Ingredient;
         //foreignObjectImage.sprite = dish.ForeignObjectImage;
 
         if (data.visionType == VisionType.None)
